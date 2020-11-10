43a1ee6fa017c6791f2e0a7746bfd2b4a67dd9f9
Othman Taik <taikothman1997@gmail.com>
2020-11-10 16:06:09 +0100

    JSnack 50%

--- JSnack-2/js/main.js
+++ JSnack-2/js/main.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+  //Creo arrey di bici con diverse proprietà
+  var bici = {
+      nome:'mountainbike',
+      peso: 75,
+    },
+    {
+      nome:'bmx',
+      peso: 50,
+    },
+    {
+      nome:'pieghevole',
+      peso: 35,
+    },
+    {
+      nome:'corsa',
+      peso: 45,
+    };
+    console.log(bici);
 
-  
+  //Stampo la bici con il peso minore
+  var lightweghtbike = bici[0];
+
+  for(var i = 1; i < bici.length; i++){
+    if(bici[i].peso < lightweghtbike.peso){
+      lightweghtbike = bici[i];
+    }
+  }
+  console.log(lightweghtbike);
+
+
 });
